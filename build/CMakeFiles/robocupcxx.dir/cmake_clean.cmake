b63087f44e2ae9c95588832b03fc728292323e38
juanmagi <juan_manuel@gimon.com>
2021-02-21 19:00:19 +0100

    En Desarrollo

---
+++ build/CMakeFiles/robocupcxx.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robocupcxx.dir/main.cpp.o"
+  "robocupcxx.pdb"
+  "robocupcxx"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robocupcxx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
